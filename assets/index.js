6e59af334f1b0ad4322fd1e8955460ed356fdd4b
Gustavo <gustavo.ovejero@educacion.local>
2024-09-16 19:51:37 -0300

    desafios 2

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+let nombre = 'Gustavo'
+
+const apellido = 'Ovejero'
+
+let edad = prompt('Ingrese su edad:')
+
+// console.log(nombre,apellido)
+// console.log("tu edad es: " + edad)
+// alert('tu edad es ' + edad)
+console.log(nombre + " " + apellido + " \n tu edad es: " + edad)
+console.log(`${nombre} ${apellido} \n tu edad es: ${edad}`)
